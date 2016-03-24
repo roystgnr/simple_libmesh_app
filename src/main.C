4cd5675712ed208a88bc7126db94033bbfdd8247
Derek Gaston <derek.gaston@inl.gov>
2016-03-24 14:23:43 -0400

    blocking send/receive works

--- src/main.C
+++ src/main.C
@@ -10,6 +10,81 @@
 #include "libmesh/numeric_vector.h"
 #include "libmesh/exodusII_io.h"
 #include "libmesh/mesh_generation.h"
+#include "libmesh/parallel.h"
+
+using namespace libMesh;
+
+class CustomClass
+{
+public:
+  CustomClass()
+  {
+    data1 = rand() / (double)RAND_MAX;
+    data2 = rand() / (double)RAND_MAX;
+  }
+
+  Real data1;
+  Real data2;
+};
+
+
+namespace libMesh {
+namespace Parallel {
+template<>
+class Packing<std::shared_ptr<CustomClass> > {
+public:
+
+  typedef Real buffer_type;
+
+  static unsigned int packed_size(typename std::vector<Real>::const_iterator in)
+  {
+    unsigned int total_size = 0;
+
+    // data1
+    total_size += 1;
+
+    // data2
+    total_size += 1;
+
+    return total_size;
+  }
+
+  static unsigned int packable_size(const std::shared_ptr<CustomClass> & cc, const void *)
+  {
+    unsigned int total_size = 0;
+
+    // data1
+    total_size += 1;
+
+    // data2
+    total_size += 1;
+
+    return total_size;
+  }
+
+  template <typename Iter>
+  static void pack (const std::shared_ptr<CustomClass> & cc, Iter data_out, const void *)
+  {
+    data_out = cc->data1;
+    data_out = cc->data2;
+  }
+
+  static std::shared_ptr<CustomClass> unpack(typename std::vector<Real>::const_iterator in, void * mesh_context)
+  {
+    std::shared_ptr<CustomClass> cc = std::make_shared<CustomClass>();
+
+    cc->data1 = (*in++);
+    cc->data2 = (*in++);
+
+    return cc;
+  }
+
+};
+
+} // namespace Parallel
+
+} // namespace libMesh
+
 
 using namespace libMesh;
 
@@ -20,37 +95,26 @@ int main (int argc, char ** argv)
 
   Parallel::Communicator & comm = init.comm();
 
-  Mesh mesh(init.comm());
+  Parallel::MessageTag tag(210);
 
-  MeshTools::Generation::build_square (mesh,
-                                       3, 3,
-                                       -1., 1.,
-                                       -1., 1.,
-                                       QUAD4);
-
-  EquationSystems equation_systems(mesh);
-
-  auto & output_system = equation_systems.add_system<ExplicitSystem>("output_system");
-
-  output_system.add_variable("processor", CONSTANT, MONOMIAL);
-
-  equation_systems.init();
-
-  auto & solution_vector = output_system.solution;
-
-  for (auto elem_it = mesh.local_elements_begin(); elem_it != mesh.local_elements_end(); ++elem_it)
+  if (comm.rank() == 0)
   {
-    auto elem = *elem_it;
+    std::vector<std::shared_ptr<CustomClass> > custom_class_vector { std::make_shared<CustomClass>(), std::make_shared<CustomClass>() };
 
-    auto dof = elem->dof_number(0, 0, 0);
+    for (auto & cc : custom_class_vector)
+      std::cerr<<comm.rank()<<" "<<cc->data1<<" "<<cc->data2<<std::endl;
 
-    solution_vector->set(dof, elem->processor_id());
+    comm.send_packed_range(1, &custom_class_vector, custom_class_vector.begin(), custom_class_vector.end(), tag);
   }
+  else
+  {
+    std::vector<std::shared_ptr<CustomClass> > custom_class_vector;
 
-  solution_vector->close();
+    comm.receive_packed_range(0, &custom_class_vector, std::back_inserter(custom_class_vector), (std::shared_ptr<CustomClass>*)(libmesh_nullptr), tag);
 
-
-  ExodusII_IO(mesh).write_equation_systems("out.e", equation_systems);
+    for (auto & cc : custom_class_vector)
+      std::cerr<<comm.rank()<<" "<<cc->data1<<" "<<cc->data2<<std::endl;
+  }
 
   return 0;
 }
